4ff4d5e7901a074641eb00dd657ac35ec7cc86d8
unknown <Kris@.(none)>
2009-09-18 21:10:09 -0600

    More work sketching out the design

--- jackconfig.js
+++ jackconfig.js
@@ -1,6 +1,10 @@
+/**
+ * The starting point for Pintura running as a Jack app.
+ */
 var pintura = require("pintura");
 
-exports.app = require("jack/cascade").Cascade([
+exports.app = require("jack/cascade").Cascade([ 
+		// cascade from static to pintura REST handling
 	require("jack/static").Static(null,{urls:[""],root:["web"]}),
 	pintura.app
 ]);

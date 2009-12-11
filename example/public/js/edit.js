ceae2972de1ba5fc6f11a9eb0e0f7f0203ae1026
Kris Zyp <kriszyp@gmail.com>
2009-12-11 07:28:25 -0700

    Adding login

--- example/public/js/edit.js
+++ example/public/js/edit.js
@@ -1,11 +1,15 @@
 var pageName = location.search.match(/page=([^&]+)/);
 pageName = pageName && pageName[1];
+document.title = "Editing " + pageName;
 request({
 	url: "Page/" + pageName,
 	headers: {
 		"accept": "application/javascript, application/json"
 	}
 }).then(function(response){
+	if(!response.headers.username){
+		login();
+	}
 	var page = eval("(" + response.body + ")");
 	if(typeof page !== "object"){
 		page = {
@@ -30,3 +34,43 @@ request({
 
 });
 
+function login(){
+	document.getElementById("login-form").style.display="block";
+	document.getElementById("sign-in").onclick = function(){
+		userRpc("authenticate").then(function(){
+			alert("Logged in");
+		}, errorHandler);
+	};
+	document.getElementById("register").onclick = function(){
+		userRpc("createUser").then(function(){
+			alert("Registered");
+		}, errorHandler);
+	};
+	function errorHandler(error){
+		alert(error);
+	}
+}
+
+function userRpc(method, params){
+	return request({
+		url: "Class/User",
+		method: "POST",
+		body: JSON.stringify({
+			id:"call-id",
+			method: method,
+			params: [
+				document.getElementById("user").value,
+				document.getElementById("password").value
+			]
+		}),
+		headers: {
+			"accept": "application/javascript, application/json"
+		}
+	}).then(function(response){
+		response = JSON.parse(response);
+		if(response.error){
+			throw response.error;
+		}
+		return response.result;
+	});
+	}

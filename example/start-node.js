8097e4132f47ad84f1e84daaeb699e92316b82eb
Kris Zyp <kriszyp@gmail.com>
2009-12-24 14:35:24 -0700

    More fixes/changes for node startup script

--- example/start-node.js
+++ example/start-node.js
@@ -7,10 +7,11 @@ require.paths.push("lib");
 require.paths.push.apply(require.paths,[
 					"packages/pintura/lib",
 					"packages/pintura/engines/node/lib",
+					"packages/pintura/engines/default/lib",
 					"packages/commonjs-utils/lib",
 					"packages/jack/lib",
 					"packages/narcissus/lib",
-					"packages/hei-jsgi/lib",
+					"packages/jsgi-node/lib",
 					"engines/default/lib",
 					"lib"
 					].map(function(path){
@@ -21,24 +22,27 @@ var sys = require("sys");
 print = sys.puts;
 global = this;
 require("global");
-
+process.addListener("uncaughtException", function(error){
+	// obviously we don't want uncaught exceptions to crash the server
+	print(error);
+});
 
 var pintura = require("pintura");
 require("app");
 
-require("hei-jsgi").run(/*
-  require("jack/cascade").Cascade([ 
+require("jsgi-node").run(
+  require("jsgi/cascade").Cascade([ 
 		// cascade from static to pintura REST handling
 	// the main place for static files accessible from the web
-	require("jack/static").Static(null, {urls:[""],root:"public"}),
+	require("jsgi/static").Static({urls:[""],root:"public"}),
 	// this will provide access to the server side JS libraries from the client
-	require("jack/static").Static(null, {urls:["/lib"],root:""}),
+	require("jsgi/static").Static({urls:["/lib"],root:""}),
 	// make the root url map to /Page/Root  
 	require("jsgi/rewriter").Rewriter(/^\/$/, "/Page/Root",
 	 	// main Pintura handler */
 		pintura.app
-	/*)
-])*/);
+	)
+]));
 
 // having a REPL is really helpful
 require("repl").start();

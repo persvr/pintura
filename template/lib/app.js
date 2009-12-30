54a844afba2b4721dc3fc88fed844a4dbedbf0d3
Kris Zyp <kriszyp@gmail.com>
2009-12-30 14:05:18 -0700

    Allow full access by default

--- template/lib/app.js
+++ template/lib/app.js
@@ -2,5 +2,5 @@
  * This is a staring point for an application written on Pintura
  */
 
-// Defines the capabilities of the users
-require("access");
+// Defines the capabilities of the users, uncomment once you define access levels
+// require("access");

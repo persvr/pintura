425754df40b7a8645654e95112d5bafd303333a6
Kris Zyp <kriszyp@gmail.com>
2009-12-30 14:04:56 -0700

    Don't reload for static files

--- example/jackconfig.js
+++ example/jackconfig.js
@@ -5,17 +5,30 @@
 var pintura = require("pintura");
 require("app");
 
-exports.app = require("jack/cascade").Cascade([ 
-		// cascade from static to pintura REST handling
-	// the main place for static files accessible from the web
-	require("jack/static").Static(null, {urls:[""],root:"public"}),
-	// this will provide access to the server side JS libraries from the client
-	require("jack/static").Static(null, {urls:["/lib"],root:""}),
+// setup the Jack application
+exports.app =
 	// make the root url redirect to /Page/Root  
-	require("jsgi/redirect-root").redirectRoot,
+	require("jsgi/redirect-root").RedirectRoot(
  	// main Pintura handler 
-	pintura.app
-]);
+		pintura.app
+	);
+
+// now setup the development environment, handle static files before reloading the app
+// for better performance
+var File = require("file");
+exports.development = function(app, options){
+	return require("jack/cascade").Cascade([ 
+			// cascade from static to pintura REST handling
+		// the main place for static files accessible from the web
+		require("jack/static").Static(null, {urls:[""],root:"public"}),
+		// this will provide access to the server side JS libraries from the client
+		require("jack/static").Static(null, {urls:["/lib"],root:""}),
+		// the typical reloader scenario
+		(!options || options.reload) ? require("jack/reloader").Reloader(File.join(File.cwd(), "jackconfig"), "app") :
+								exports.app
+	]);
+};
 
 // having a REPL is really helpful
 new (require("worker").SharedWorker)("narwhal/repl");
+

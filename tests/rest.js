e9b43b725a24c0cb400556118a9ed9664c45c997
Kris Zyp <kriszyp@gmail.com>
2010-02-16 10:43:11 -0700

    Add performance test

--- tests/rest.js
+++ tests/rest.js
@@ -14,6 +14,17 @@ exports.testGet = function(){
 	assert.equal(parse(body).length, 3);
 };
 
+exports.testPerformance = function(){
+	var startTime = new Date().getTime();
+	for(var i = 0;i < 1000;i++){
+		mock.GET("/Class/TestStore", {
+			headers:{
+				authorization: "user:pass"
+			}
+		});
+	}
+	print("Performed " + 1000000 / (new Date().getTime() - startTime) + " requests per second");
+}
 if (require.main == module)
     require("os").exit(require("test/runner").run(exports));
 

d299c2315394fb252116e272d6e73734aaaef266
Kris Zyp <kriszyp@gmail.com>
2010-03-14 21:19:43 -0600

    Switch to using transporter + require.js for client side module loading

--- example/jackconfig.js
+++ example/jackconfig.js
@@ -17,12 +17,12 @@ exports.app =
 // for better performance
 var File = require("file");
 exports.development = function(app, options){
-	return require("jack/cascade").Cascade([ 
+	return require("jack/cascade").Cascade([
 			// cascade from static to pintura REST handling
 		// the main place for static files accessible from the web
 		require("jack/static").Static(null, {urls:[""],root:"public"}),
 		// this will provide access to the server side JS libraries from the client
-		require("jack/static").Static(null, {urls:["/lib"],root:""}),
+		require("jsgi/transporter").Transporter(),
 		// the typical reloader scenario
 		(!options || options.reload) ? require("jack/reloader").Reloader(File.join(File.cwd(), "jackconfig"), "app") :
 								exports.app

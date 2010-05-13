181ac73f6e3125b40f9b206a32d0e858f55dd20a
Kris Zyp <kriszyp@gmail.com>
2010-05-12 22:38:09 -0600

    Moved template to a new project

--- template/lib/app.js
+++
@@ -1,6 +0,0 @@
-/**
- * This is a staring point for an application written on Pintura
- */
-
-// Defines the capabilities of the users, uncomment once you define access levels
-// require("access");
--- template/public/css/README.md
+++
@@ -1,1 +0,0 @@
-This is where CSS files go

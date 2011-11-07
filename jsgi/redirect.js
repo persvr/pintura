f4a65a8f4c3c015053aa0c08192e860e7399bd57
Kris Zyp <kriszyp@gmail.com>
2011-11-07 11:20:14 -0700

    Add redirect middleware module

---
+++ jsgi/redirect.js
@@ -0,0 +1,29 @@
+var uri = require("../util/url");
+
+exports.Redirect = function (path, status) {
+    
+    status = status || 301;
+    
+    return function (request) {
+        var location =
+            (request.scheme || "http") +
+            "://" +
+            (request.headers.host || (
+                request.host +
+                (request.port == 80 ? "" : ":" + request.port)
+            )) +
+            (request.scriptName || "") +
+            request.pathInfo;
+        
+        location = path ? uri.resolve(location, path) : request.headers.referer;
+        
+        return {
+            status: status,
+            headers: {
+                "location": location,
+                "content-type": "text/plain"
+            },
+            body: ['Go to <a href="' + location + '">' + location + "</a>"]
+        }
+    }
+}

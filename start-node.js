cc7faf4354e52ed1a36c069ec8ded242962e0d1a
ben hockey <neonstalwart@gmail.com>
2014-09-24 10:21:20 -0500

    remove websocket-server
    working on replacing it with socket.io

--- start-node.js
+++ start-node.js
@@ -1,6 +1,5 @@
 // helpful for debugging
 var settings = require("perstore/util/settings"),
-	ws = require("websocket-server"),
 	messageJson = require("./media/message/json");
 
 exports.start = function(jsgiApp){
@@ -9,16 +8,6 @@ exports.start = function(jsgiApp){
 		);
 	var port = settings.port || process.env.PORT || 80;
 	server.listen(port);
-	require("jsgi-node/ws-jsgi")(ws.createServer({
-		server: server
-	}), function(request){
-		request.method = "POST";
-		var headers = request.headers;
-		headers.accept = "message/json";
-		headers["content-type"] = "message/json";
-		headers.stream = true;
-		return jsgiApp(request);
-	});
 	
 	console.log("Listening on port " + port);
 	return server;

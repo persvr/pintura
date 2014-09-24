e04480ce261714b6d67e16643c11845b2d7e55e8
ben hockey <neonstalwart@gmail.com>
2014-09-24 16:43:45 -0500

    switch to use ws instead of websocket-server
    websocket-server is unpublished so updating to an alternative

--- start-node.js
+++ start-node.js
@@ -1,5 +1,6 @@
 // helpful for debugging
 var settings = require("perstore/util/settings"),
+	ws = require("ws"),
 	messageJson = require("./media/message/json");
 
 exports.start = function(jsgiApp){
@@ -8,7 +9,15 @@ exports.start = function(jsgiApp){
 		);
 	var port = settings.port || process.env.PORT || 80;
 	server.listen(port);
-	
+	require("jsgi-node/ws-jsgi")(ws.createServer({ server: server }), function(request){
+		request.method = "POST";
+		var headers = request.headers;
+		headers.accept = "message/json";
+		headers["content-type"] = "message/json";
+		headers.stream = true;
+		return jsgiApp(request);
+	});
+
 	console.log("Listening on port " + port);
 	return server;
 };
